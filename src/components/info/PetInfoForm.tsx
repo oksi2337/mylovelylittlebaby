da05b7c032b17a37e84a82f7465235a6e87ca9c5
oksi2337 <oksi2337@gmail.com>
2026-04-24 17:37:32 +0900

    auto: update files

--- src/components/info/PetInfoForm.tsx
+++ src/components/info/PetInfoForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import type { FieldError, Merge } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
